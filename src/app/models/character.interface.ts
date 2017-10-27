6ae7e095f6edc517935418cfdfed39a7d9c3d3f4
alexandrejuk <ale_santos.soares@hotmail.com>
2017-10-27 15:57:35 -0200

    fix: modify name from interface

---
+++ src/app/models/character.interface.ts
@@ -0,0 +1,25 @@
+export interface Character {
+  id: number;
+  name: string;
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: Wand;
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alive: boolean;
+  image: string;
+}
+
+interface Wand {
+  wood: string;
+  core: string;
+  length: number;
+}
